28de2d0c8a88f40542e376f6899f76fa27b400b4
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-04-01 22:08:26 +0600

    Create stl c++ all is function.cpp

---
+++ stl c++ all is function.cpp
@@ -0,0 +1,18 @@
+
+Here is a list of C++ STL functions that begin with the prefix "is":
+
+isalnum(): checks if a character is alphanumeric.
+isalpha(): checks if a character is alphabetic.
+isblank(): checks if a character is blank (either a space or a tab).
+iscntrl(): checks if a character is a control character.
+isdigit(): checks if a character is a digit.
+isgraph(): checks if a character has a graphical representation.
+islower(): checks if a character is lowercase.
+isprint(): checks if a character is printable (has a graphical representation or is a space character).
+ispunct(): checks if a character is a punctuation character.
+isspace(): checks if a character is a whitespace character.
+isupper(): checks if a character is uppercase.
+isxdigit(): checks if a character is a hexadecimal digit.
+All of these functions are defined in the <cctype> header, and they take a single int parameter that represents the character to be tested. Each function returns a non-zero value if the character satisfies the condition, and 0 otherwise.
+
+Note that some of these functions have similar functionality. For example, isgraph() returns true for any character that is not whitespace, whereas isprint() returns true for any character that is either printable or a space character. Similarly, isupper() and islower() are complementary functions that check for uppercase and lowercase characters, respectively.
